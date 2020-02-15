abb28bd052bdb309286a8db34fd848492d04e997
vinicius-vasconcelos <viniciussouzav@gmail.com>
2020-02-15 01:49:35 -0300

    Estilizando o projeto1 e fazendo os métodos

--- projeto1/app.js
+++ projeto1/app.js
@@ -2,10 +2,16 @@ const vm = new Vue({
     el: '#app',
     data: {
         produtos: [],
-        produto: 'false'
+        produto: false,
+        carrinho: [],
+        mensagemAlerta: 'Item adicionado',
+        alertaAtivo: false,
     },
-    filters: {
-        maskPrice(valor) { return valor.toLocaleString('pt-br', { style: 'currency', currency: 'BRL' }) }
+    filters: { maskPrice(valor) { return valor.toLocaleString('pt-br', { style: 'currency', currency: 'BRL' }) } },
+    created() {
+        this.fetchProdutos();
+        this.checkLocalStorage();
+        this.router();
     },
     methods: {
         fetchProdutos() {
@@ -17,9 +23,50 @@ const vm = new Vue({
             fetch(`./api/produtos/${id}/dados.json`)
                 .then(r => r.json())
                 .then(prod => this.produto = prod)
+        },
+        openModal(id) {
+            this.fetchProduto(id);
+            window.scrollTo({
+                top: 0,
+                behavior: 'smooth'
+            })
+        },
+        closeModal({ target, currentTarget }) { if (target === currentTarget) this.produto = false; },
+        addItem() {
+            this.produto.estoque--;
+            const { id, nome, preco } = this.produto;
+            this.carrinho.push({ id, nome, preco });
+            this.alerta(`${nome} foi adicionado ao carrinho.`)
+        },
+        removeItem(index) { this.carrinho.splice(index, 1) },
+        checkLocalStorage() { if (window.localStorage.carrinho) this.carrinho = JSON.parse(window.localStorage.carrinho) },
+        alerta(msg) {
+            this.mensagemAlerta = msg;
+            this.alertaAtivo = true;
+            setTimeout(() => { this.alertaAtivo = false }, 1500)
+        },
+        router() {
+            const hash = document.location.hash;
+
+            if (hash)
+                this.fetchProduto(hash.replace('#', ''))
         }
     },
-    created() {
-        this.fetchProdutos();
+    computed: {
+        carrinhoTotal() {
+            let total = 0;
+            if (this.carrinho.length)
+                this.carrinho.forEach(element => total += element.preco);
+            return total;
+        }
+    },
+    watch: {
+        produto() {
+            document.timeline = this.produto.nome || "Techno";
+            const hash = `#${this.produto.id}` || "";
+            history.pushState(null, null, hash);
+        },
+        carrinho() { window.localStorage.carrinho = JSON.stringify(this.carrinho) }
     }
+
 })
